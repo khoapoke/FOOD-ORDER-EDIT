7f1f87d9b377541f798552982d919f84c8b6af2a
OXplakhoa <accbangbangchuot@gmail.com>
2025-06-09 15:30:29 +0700

    Add AdminPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Header from "./Components/header/Header.jsx";
-import Footer from "./Components/footer/Footer.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import Content from "./Components/content/Content.jsx";
+import AdminPage from "./components/admin/AdminPage.jsx";
+
 function App() {
   return (
-    <>
-      <Header />
-      <Content />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header />
+            <Content />
+            <Footer />
+          </>
+        } />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
