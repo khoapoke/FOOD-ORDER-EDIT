4836263cb9337cf3cd8206da86195e3d1c2906c6
khoapoke <minhkhoa04042005@gmail.com>
2025-06-13 17:20:17 +0700

    chỉnh lại tên components viết hoa chữ C

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout.jsx";
 import AuthLayout from "./Layout/AuthLayout.jsx";
 import Menu from "./menu/Menu.jsx";
-import AdminPage from "./components/admin/AdminPage.jsx";
+import AdminPage from "./Components/admin/AdminPage.jsx";
 import AdminSidebar from "./Components/admin/AdminSideBar.jsx";
 import LoginPage from "./Components/auth/Login.jsx";
 import Content from "./Components/content/Content.jsx";
-
+import AboutUs1 from "./aboutus/AboutUs1.jsx";
+import AboutUs2 from "./aboutus/AboutUs2.jsx";
 function App() {
   return (
     <>
@@ -43,6 +44,23 @@ function App() {
                 <AdminPage />
               </AdminSidebar>
             </AuthLayout>
+          }
+        />
+        <Route
+          path="/aboutus/1"
+          element={
+            <MainLayout>
+              <AboutUs1 />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/aboutus/2"
+          element={
+            <MainLayout>
+              <AboutUs2 />
+            </MainLayout>
           }
         />
       </Routes>
