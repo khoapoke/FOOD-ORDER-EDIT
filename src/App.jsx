140b167dbc7a936c17918c8915644e1b84eb0895
khoapoke <minhkhoa04042005@gmail.com>
2025-06-13 09:04:52 +0700

    chỉnh lại đường dẫn, Mainlayout có sẵn header footer, Authlayout ,dành cho admin không có hder,foter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout.jsx";
 import AuthLayout from "./Layout/AuthLayout.jsx";
 import Menu from "./menu/Menu.jsx";
-import AdminPage from "./Components/admin/AdminPage.jsx";
+import AdminPage from "./components/admin/AdminPage.jsx";
 import AdminSidebar from "./Components/admin/AdminSideBar.jsx";
 import LoginPage from "./Components/auth/Login.jsx";
 import Content from "./Components/content/Content.jsx";
